b712b46f90127fcd87da394761970c502b974489
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-01-18 09:53:13 +0100

    eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,23 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'plugin:vue/vue3-essential',
-        'airbnb-base',
-    ],
-    overrides: [
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: [
-        'vue',
-    ],
-    rules: {
-        'linebreak-style': 'off',
-        indent: ['error', 4],
-    },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'airbnb-base',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'linebreak-style': 'off',
+    indent: ['error', 2],
+  },
 };
