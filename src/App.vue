d1fb5a041c7ecd9832e038392541b1cc4bd19ef4
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-01-18 13:11:32 +0100

    made bg into component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <script setup>
 // import content from './components/ContextText.vue';
 import circleShimmer from './components/circleShimmer.vue';
+import background from './components/backgroundImage.vue';
 
-const background = document.getElementsByClassName('imageContainer');
+/*
 
-document.onmousemove = function followMouse() {
-  const x = `${(event.clientX * 5) / window.innerWidth}%`;
-  const y = `${(event.clientY * 5) / window.innerHeight}%`;
-
-  for (let i = 0; i < 2; i += 1) {
-    background[i].style.left = x;
-    background[i].style.top = y;
-  }
-};
+*/
 </script>
 
 <template>
   <div class="background">
-    <div class="imageContainer"></div>
+    <background />
   <circleShimmer class="circleShimmer" />
   </div>
 </template>
@@ -30,15 +23,7 @@ document.onmousemove = function followMouse() {
   width: 100vw;
   margin: 0;
 }
-.imageContainer {
-  background-image: url('assets/img/trees-background.jpg');
-  position: absolute;
-  min-height: 100%;
-  min-width: 100%;
-  object-fit: cover;
-  transform-origin: middle center;
-  transform: scale(1.2);
-}
+
 .circleShimmer {
   margin: auto;
 }
---
+++ src/components/backgroundImage.vue
@@ -0,0 +1,29 @@
+<script>
+
+const background = document.getElementsByClassName('imageContainer');
+
+function followMouse(e) {
+  const x = `${(e.clientX * 5) / window.innerWidth}%`;
+  const y = `${(e.clientY * 5) / window.innerHeight}%`;
+
+  for (let i = 0; i < 2; i += 1) {
+    background[i].style.left = x;
+    background[i].style.top = y;
+  }
+}
+document.addEventListener('mousemove', followMouse);
+</script>
+<template>
+<div class="imageContainer"></div>
+</template>
+<style scoped lang="scss">
+.imageContainer {
+  background-image: url('../assets/img/trees-background.jpg');
+  position: absolute;
+  min-height: 100%;
+  min-width: 100%;
+  object-fit: cover;
+  transform-origin: middle center;
+  transform: scale(1.2);
+}
+</style>
